c9869255866ca02ace008e1fbcbacf54736c1807
hsmcadam <135244079+hsmcadam@users.noreply.github.com>
2023-06-16 10:51:41 +0000

    Update main.js

--- main.js
+++ main.js
@@ -38,47 +38,51 @@ function confirm(){
     flex.setAttribute('class','flex');
 
     //name
-    var spanName=document.createElement("div");
-    spanName.setAttribute('class','name');
-    spanName.innerHTML=goalName;
+    var divName=document.createElement("div");
+    divName.setAttribute('class','name');
     para.appendChild(flex);
-    flex.appendChild(spanName);
-    spanName.setAttribute('onclick', "this.parentElement.parentElement.querySelector('.description').innerHTML=this.parentElement.parentElement.id;");
-    
+    flex.appendChild(divName);
+    divName.innerHTML=goalName;
+
     //new
-    var spanNew=document.createElement("div");
+    var space=document.createElement("div");
+    space.setAttribute('class','space');
+    flex.appendChild(space);
+
+    var spanNew=document.createElement("span");
     spanNew.innerHTML=" New";
     spanNew.setAttribute('class','new');
-    flex.appendChild(spanNew);
+    divName.appendChild(spanNew);
+
 
     //done
     var numberS =idFind();
-    var spanDone=document.createElement("div");
-    spanDone.innerHTML=" Complete ";
-    spanDone.setAttribute('class','done');
-    spanDone.addEventListener('click', function() {
-        if(spanDone.innerHTML!=""){
-        spanDone.style.color='grey';
+    var divDone=document.createElement("div");
+    divDone.innerHTML=" Complete ";
+    divDone.setAttribute('class','done');
+    divDone.addEventListener('click', function() {
+        if(divDone.innerHTML!=""){
+        divDone.style.color='#grey';
         //spanDone.style.background='grey';
         para.style.background='grey';
 
         para.classList.toggle('checked');
-        spanDone.classList.toggle('checked');
-        spanDone.innerHTML=""
+        divDone.classList.toggle('checked');
+        divDone.innerHTML=""
 
-        spanName.classList.toggle('checkedName');
+        divName.classList.toggle('checkedName');
         }
         })
-    spanDone.setAttribute('id',numberS);
+    divDone.setAttribute('id',numberS);
     //spanDone.setAttribute('onclick',"document.getElementsByClassName('name')[id].parentNode.style.background='grey';");
-    flex.appendChild(spanDone);
+    flex.appendChild(divDone);
 
     //close
-    var spanClose=document.createElement("div");
-    spanClose.setAttribute('class','close');
-    spanClose.setAttribute("onclick","this.parentElement.parentElement.style.display='none';")
-    spanClose.innerHTML='&times;';
-    flex.appendChild(spanClose);
+    var divClose=document.createElement("div");
+    divClose.setAttribute('class','close');
+    divClose.setAttribute("onclick","this.parentElement.parentElement.style.display='none';")
+    divClose.innerHTML='&times;';
+    flex.appendChild(divClose);
 
 
     //description
@@ -86,7 +90,9 @@ function confirm(){
     descriptionDiv.setAttribute('class','description');
     var idDescription="description"+numberS;
     descriptionDiv.setAttribute('id',idDescription);
-    para.appendChild(descriptionDiv);
+    divName.appendChild(descriptionDiv);
+    descriptionDiv.innerHTML=goalDescription;
+
     //para.setAttribute("onclick","document.getElementById(document.getElementById(this.id).querySelector('.description').id).innerHTML=id;")
     
     findNew();
@@ -101,7 +107,10 @@ function confirm(){
 
 function findNew(){
     if( document.getElementsByClassName('new')[0]!=null){
-        document.getElementsByClassName('new')[0].parentElement.classList.toggle('old');
-        document.getElementsByClassName('new')[0].remove();}
+        document.getElementsByClassName('new')[0].innerHTML=""
+        document.getElementsByClassName('new')[0].classList.add('old');
+        document.getElementsByClassName('new')[0].classList.remove('new');
+
+    }
 }
 
